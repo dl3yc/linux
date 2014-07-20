a67685595f109e358c43c70757ca6e3d206fcffa
Sebastian Weiss <dl3yc@darc.de>
2014-07-20 20:20:24 +0200

    ASoC: BCM2708: Add support for memory maping and remove 24 bit support

--- sound/soc/bcm/bcm2708-i2s.c
+++ sound/soc/bcm/bcm2708-i2s.c
@@ -374,10 +374,6 @@ static int bcm2708_i2s_hw_params(struct snd_pcm_substream *substream,
 		data_length = 16;
 		bclk_ratio = 40;
 		break;
-	case SNDRV_PCM_FORMAT_S24_LE:
-		data_length = 24;
-		bclk_ratio = 40;
-		break;
 	case SNDRV_PCM_FORMAT_S32_LE:
 		data_length = 32;
 		bclk_ratio = 80;
@@ -456,7 +452,7 @@ static int bcm2708_i2s_hw_params(struct snd_pcm_substream *substream,
 	/* Setup the frame format */
 	format = BCM2708_I2S_CHEN;
 
-	if (data_length >= 24)
+	if (data_length > 24)
 		format |= BCM2708_I2S_CHWEX;
 
 	format |= BCM2708_I2S_CHWID((data_length-8)&0xf);
@@ -746,7 +742,6 @@ static struct snd_soc_dai_driver bcm2708_i2s_dai = {
 		.channels_max = 2,
 		.rates =	SNDRV_PCM_RATE_8000_192000,
 		.formats =	SNDRV_PCM_FMTBIT_S16_LE
-				// | SNDRV_PCM_FMTBIT_S24_LE : disable for now, it causes white noise with xbmc
 				| SNDRV_PCM_FMTBIT_S32_LE
 		},
 	.capture = {
@@ -754,7 +749,6 @@ static struct snd_soc_dai_driver bcm2708_i2s_dai = {
 		.channels_max = 2,
 		.rates =	SNDRV_PCM_RATE_8000_192000,
 		.formats =	SNDRV_PCM_FMTBIT_S16_LE
-				| SNDRV_PCM_FMTBIT_S24_LE
 				| SNDRV_PCM_FMTBIT_S32_LE
 		},
 	.ops = &bcm2708_i2s_dai_ops,
@@ -819,10 +813,11 @@ static const struct snd_soc_component_driver bcm2708_i2s_component = {
 };
 
 static const struct snd_pcm_hardware bcm2708_pcm_hardware = {
-	.info			= SNDRV_PCM_INFO_INTERLEAVED |
+	.info			= SNDRV_PCM_INFO_MMAP |
+				  SNDRV_PCM_INFO_MMAP_VALID |
+				  SNDRV_PCM_INFO_INTERLEAVED |
 				  SNDRV_PCM_INFO_JOINT_DUPLEX,
 	.formats		= SNDRV_PCM_FMTBIT_S16_LE |
-				  SNDRV_PCM_FMTBIT_S24_LE |
 				  SNDRV_PCM_FMTBIT_S32_LE,
 	.period_bytes_min	= 32,
 	.period_bytes_max	= 64 * PAGE_SIZE,
